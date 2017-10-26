aa79c8da78cd8a45df3c7c4f032acfde98f274f1
Derek Johnson <d.johnson13879@gmail.com>
2017-10-26 08:50:24 -0400

    first commit

---
+++ reactnotes/src/Note/Note.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Note.css';
+
+class Note extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.noteContent = props.noteContent;
+		this.noteId = props.noteId;
+		this.handleRemoveNote = this.handleRemoveNote.bind(this);
+	}
+
+	handleRemoveNote(id) {
+		this.props.removeNote(id);
+	}
+
+	render(props){
+		return(
+			<div className='note fade-in'>
+				<span className='closebtn' 
+					  onClick={() => this.handleRemoveNote(this.noteId)}>
+					  &times;
+				</span>
+				<p className='noteContent'>{ this.noteContent }</p>
+			</div>
+		)
+	}
+}
+
+export default Note;
